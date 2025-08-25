93d1f634b0c9849997c75f79c9d7196aa0bac00a
Shwetaraut007 <rautshweta19125@gmail.com>
2025-08-25 06:04:42 -0700

    Add files via upload

---
+++ addition, subtraction.python.py
@@ -0,0 +1,16 @@
+Python 3.13.4 (tags/v3.13.4:8a526ec, Jun  3 2025, 17:46:04) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> >>> a=15
+... >>> print(a)
+... 15
+... >>>
+... >>> a=55
+... >>> b=11
+... >>> c=a+b
+... >>> print(c)
+... 66
+... >>>
+... >>> a=55
+... >>> b=45
+... >>> c=a-b
+... >>> print(c)
